dce4b04c7d3a21ed926fa252aa0e636614c32f6c
Maksim Kanev <maks_kv@yahoo.com>
2023-05-10 21:58:58 +0300

    Added employee helper

---
+++ Helpers/EmployeeHelper.cs
@@ -0,0 +1,37 @@
+﻿using Human_Resource_Management_App.Entities;
+
+namespace Human_Resource_Management_App.Helpers
+{
+    public interface IEmployeeHelper
+    {
+        public Employee CreateEmployeeEntity(string firstName, string lastName, string department, decimal salary);
+        public bool CheckEmployeeID(int id);
+    }
+    public class EmployeeHelper : IEmployeeHelper
+    {
+        public Employee CreateEmployeeEntity(string firstName, string lastName, string department, decimal salary)
+        {
+            if(firstName == null ||  lastName == null || department == null || salary == 0)
+                return null;
+
+            Employee newEmployee = new Employee();
+
+            newEmployee.FirstName = firstName; 
+            newEmployee.LastName = lastName;
+            newEmployee.Department = department;
+            newEmployee.Salary = salary;
+
+            return newEmployee;
+        }
+
+        public bool CheckEmployeeID(int id)
+        {
+            if(id == 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}

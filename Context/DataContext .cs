e20c9f472ea50da36b820b29523c58da3fe4de7b
Maksim Kanev <maks_kv@yahoo.com>
2023-05-10 21:56:57 +0300

    Added database context

---
+++ Context/DataContext .cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Human_Resource_Management_App.Entities;
+using System.Diagnostics;
+
+namespace Human_Resource_Management_App.Context
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
+

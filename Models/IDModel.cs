551ffd5007c1c55e26db3ae9143f8be2e68de726
Maksim Kanev <maks_kv@yahoo.com>
2023-05-10 21:57:57 +0300

    Added ID model

---
+++ Models/IDModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Human_Resource_Management_App.Models
+{
+    public class IDModel
+    {
+        [Required]
+        public int Id { get; set; }
+    }
+}

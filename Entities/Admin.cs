c49c93a6199975d3744d6c090e9378ecb40fc797
Maksim Kanev <maks_kv@yahoo.com>
2023-05-10 21:57:22 +0300

    Added admin entity

---
+++ Entities/Admin.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Human_Resource_Management_App.Entities
+{
+    [Table("Admin")]
+    public class Admin
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public int? EmployeeId { get; set; }
+
+        public virtual Employee Employee { get; set; }
+    }
+}

551c506b8691ed35780e8847224666e78be6fe52
Maksim Kanev <maks_kv@yahoo.com>
2023-05-10 21:58:05 +0300

    Added employee model

---
+++ Models/EmployeeModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Human_Resource_Management_App.Models
+{
+    public class EmployeeModel : EmployeeBaseModel
+    {
+        [Required]
+        public int Id { get; set; }
+    }
+}

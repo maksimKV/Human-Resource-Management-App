5993aba967d777b004c515a0e6abbf134927fb50
Maksim Kanev <maks_kv@yahoo.com>
2023-05-10 21:58:24 +0300

    Added authentication model

---
+++ Models/AuthenticateModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Human_Resource_Management_App.Models
+{
+    public class AuthenticateModel
+    {
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}

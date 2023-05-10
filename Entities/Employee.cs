cafefd8932ff19a231fc0e7151d83fbf840c127b
Maksim Kanev <maks_kv@yahoo.com>
2023-05-10 21:57:29 +0300

    Added employee entity

---
+++ Entities/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Human_Resource_Management_App.Entities
+{
+    [Table("Employee")]
+    public class Employee
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Department { get; set; }
+        public decimal Salary { get; set; }
+
+        public virtual Admin Admin { get; set; }
+    }
+}

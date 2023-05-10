02d7e3d110a23f67a16f2034cf014f2f1f84eb21
Maksim Kanev <maks_kv@yahoo.com>
2023-05-10 21:59:10 +0300

    Added extension methods

---
+++ Helpers/ExtensionMethods.cs
@@ -0,0 +1,13 @@
+﻿using Human_Resource_Management_App.Entities;
+
+namespace Human_Resource_Management_App.Helpers
+{
+    public static class ExtensionMethods
+    {
+        public static Admin WithoutPassword(this Admin admin)
+        {
+            admin.Password = null;
+            return admin;
+        }
+    }
+}
